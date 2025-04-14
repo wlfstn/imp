da45bccd8c5f2c854458692839da1c7789d8fc59
CorpoWolf <gmangavin1@gmail.com>
2025-04-13 17:04:53 -0700

    readme & docs update

--- docs/install.ps1
+++ docs/install.ps1
@@ -1,17 +1,3 @@
-# The Wolfstone IMP
-Short for "Imperial" as in the imperial measurement system primarily used in the United States as well as some industries globally.
-
-## What does IMP provide?
-- Split feet/inch inputs evenly into feet/inch (`imp -d 8f8 -s 4`)
-- Get total inches from feet/inch input (`impt -in -d 8f8`)
-
-## Example
-`imp -d 8f6i -s 2` this will divide 8'6" into two equal sections resulting in 4'3"
-`imp -d 14f -s 4` this will divide 14'0" into four equal sections resulting in 3'6"
-Note the "i" is optional
-
-# Quick Install with Powershell
-```powershell
 $exeDownload = "https://github.com/wlfstn/imp/releases/download/V1.2/imp.exe"
 $destinationDir = "$env:USERPROFILE\wlfstn\imp"
 $destinationFile = Join-Path $destinationDir "imp.exe"
@@ -43,4 +29,3 @@ if ($userPath -split ";" -contains $destinationDir) {
 		Write-Host "Installed, but skipped adding to PATH."
 	}
 }
-```
